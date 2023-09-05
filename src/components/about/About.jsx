afb4cb65e8130ca661878b5a6e8e6d085f63a5a7
Vedant Singhal <vedant759@gmail.com>
2023-09-05 13:11:35 +0530

    third commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
           <div className="download-cv">
             <a
               className="view-cv"
-              href="https://drive.google.com/file/d/19mAdIVGWY3hQii6yAsjwgF2d964keEzi/view?usp=sharing"
+              href="https://drive.google.com/file/d/1pzn9nSv0q3uglE3VDzBAJYn1TUvL7tvM/view?usp=sharing"
               download
               target="-blank"
             >
